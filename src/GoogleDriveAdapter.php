2b51d4ec88ac242ea2987203beefd28553548f3c
as247 <as247@vui360.com>
2024-01-31 22:49:43 +0700

    v2.0 which support league/flysystem 2.x

--- src/GoogleDriveAdapter.php
+++ src/GoogleDriveAdapter.php
@@ -11,18 +11,20 @@ use As247\CloudStorages\Storage\GoogleDrive;
 
 use As247\Flysystem\DriveSupport\StorageToAdapter;
 use Google_Service_Drive;
-use League\Flysystem\Adapter\AbstractAdapter;
+use League\Flysystem\FilesystemAdapter;
+use League\Flysystem\PathPrefixer;
 
-class GoogleDriveAdapter extends AbstractAdapter
+class GoogleDriveAdapter implements FilesystemAdapter
 {
 	use StorageToAdapter;
-
+    protected $prefixer;
     public function __construct(Google_Service_Drive $service, $options = [])
     {
 		if(!is_array($options)){
 			$options=['root'=>$options];
 		}
         $this->storage = new GoogleDrive($service,$options);
+        $this->prefixer = new PathPrefixer($options['prefix']??'', DIRECTORY_SEPARATOR);
         $this->throwException=$options['debug']??'';
     }
 }
