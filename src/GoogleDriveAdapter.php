95aafb99f808ddfe14a44026178b6862bbb94c60
as247 <as247@vui360.com>
2024-03-01 17:34:06 +0700

    Update v1 to match new structure

--- src/GoogleDriveAdapter.php
+++ src/GoogleDriveAdapter.php
@@ -7,15 +7,15 @@
  */
 
 namespace As247\Flysystem\GoogleDrive;
-use As247\CloudStorages\Storage\GoogleDrive;
 
-use As247\Flysystem\DriveSupport\StorageToAdapter;
+use As247\CloudStorages\Storage\GoogleDrive;
+use As247\CloudStorages\Support\StorageToAdapterV1;
 use Google_Service_Drive;
 use League\Flysystem\Adapter\AbstractAdapter;
 
 class GoogleDriveAdapter extends AbstractAdapter
 {
-	use StorageToAdapter;
+	use StorageToAdapterV1;
 
     public function __construct(Google_Service_Drive $service, $options = [])
     {
